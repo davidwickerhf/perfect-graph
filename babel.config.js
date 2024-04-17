1ba73f4bab55187b4135187d8805f0d7efaf1837
David Henry Francis Wicker <davidwickerhf@gmail.com>
2024-04-17 13:52:20 +0200

    Update dependencies. Breaking changes!

--- babel.config.js
+++ babel.config.js
@@ -28,10 +28,14 @@ module.exports = function (api) {
           legacy: true
         }
       ],
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread'
+      '@babel/plugin-transform-nullish-coalescing-operator', // updated
+      '@babel/plugin-transform-optional-chaining', // updated
+      '@babel/plugin-transform-class-properties', // updated
+      '@babel/plugin-transform-object-rest-spread', // updated
+      '@babel/plugin-transform-optional-catch-binding', // add this new plugin
+      '@babel/plugin-transform-logical-assignment-operators', // add this new plugin
+      '@babel/plugin-transform-numeric-separator', // add this new plugin
+      '@babel/plugin-transform-async-generator-functions' // add this new plugin
     ]
   }
 }
